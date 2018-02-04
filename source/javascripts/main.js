0a705ca91d5f3eb5bc24ee0ffd6f7639a5bfd534
Vitalii Petko <vitaliypetko@gmail.com>
2018-02-04 03:45:52 +0200

    update 04.02

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -1,3 +1,20 @@
 $( document ).ready(function() {
+	var nav_h = $('.nav').height(),
+			off = $('.nav').offset().top;
 
+	$(window).scroll(function() {
+		var scr = $(this).scrollTop();
+		if (scr > off) {
+			$('body').addClass('fixed');
+		} else {
+			$('body').removeClass('fixed');
+		}
+	});
+
+	$('.nav__main-menu--mobile-btn').click(function(event) {
+		$('.nav__main-menu--mobile').slideDown('400');
+	});
+	$('.nav__main-menu--mobile .close').click(function(event) {
+		$('.nav__main-menu--mobile').slideUp('400');
+	});
 });
